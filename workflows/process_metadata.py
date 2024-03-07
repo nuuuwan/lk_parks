b19215914d0384ae9bff00ac3012c30154c3e6db
nuuuwan <nuuuwan@gmail.com>
2024-03-07 14:34:50 +0530

    Removed print

--- workflows/process_metadata.py
+++ workflows/process_metadata.py
@@ -4,8 +4,7 @@ from lk_parks import MetaData
 def main():
     MetaData.save_all()
     MetaData.build_readme()
-    for md in MetaData.list_all():
-        print(md)
+
 
 
 if __name__ == "__main__":
