854dd8284a047c52607a463ed0e1f3c84cf9af86
nuuuwan <nuuuwan@gmail.com>
2024-03-08 12:27:55 +0530

    Fixed duplicates BUG

--- src/lk_parks/metadata/MetaDataList.py
+++ src/lk_parks/metadata/MetaDataList.py
@@ -12,8 +12,9 @@ class MetaDataList:
 
     @property
     def cmp(self) -> int:
+        lat = self.latlng[0]
         return '-'.join([self.family, self.genus,
-                        self.scientific_name, str(self.latlng[0])])
+                        self.scientific_name, f'{lat:.4f}'])
 
     @classmethod
     @cache
@@ -29,32 +30,6 @@ class MetaDataList:
                 md_list.append(md)
         md_list = sorted(md_list, key=lambda md: md.cmp)
         return md_list
-
-    @classmethod
-    @cache
-    def summary(cls) -> dict:
-        md_list = cls.list_all()
-
-        def get_key_to_n(key_lambda):
-            key_to_n = {}
-            for md in cls.list_all():
-                key = key_lambda(md)
-                if key not in key_to_n:
-                    key_to_n[key] = 0
-                key_to_n[key] += 1
-            sorted_key_to_n = dict(
-                sorted(
-                    key_to_n.items(),
-                    key=lambda item: item[1],
-                    reverse=True))
-            return sorted_key_to_n
-
-        return dict(
-            n=len(md_list),
-            family_to_n=get_key_to_n(lambda md: md.family),
-            genus_to_n=get_key_to_n(lambda md: f'{md.genus} ({md.family})'),
-            species_to_n=get_key_to_n(lambda md: md.scientific_name),
-        )
 
     @classmethod
     @cache
