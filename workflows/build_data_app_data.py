96f783b01fea2645e2cd0e140f582fd97da7d32f
nuuuwan <nuuuwan@gmail.com>
2024-03-12 09:06:01 +0530

    merged

--- workflows/build_data_app_data.py
+++
@@ -1,9 +0,0 @@
-from lk_parks.core.data_app.DataApp import DataApp
-
-
-def main():
-    DataApp().write_all()
-
-
-if __name__ == "__main__":
-    main()
--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,10 +1,11 @@
 from lk_parks import PlantNetResult, PlantPhoto
-
+from lk_parks.core.data_app.DataApp import DataApp
 
 def main():
     PlantPhoto.build_from_dir_data_original_image()
     PlantNetResult.build_from_plant_photos()
     PlantPhoto.build_contents()
+    DataApp().write_all()
 
 
 if __name__ == "__main__":
