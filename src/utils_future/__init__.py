66b3bf288ad6ea4f7ebc414bad4536d7ce28c4fc
nuuuwan <nuuuwan@gmail.com>
2024-03-21 10:19:49 +0530

    Added Markdown

--- src/utils_future/__init__.py
+++ src/utils_future/__init__.py
@@ -18,3 +18,41 @@ class LatLng:
             lat=d['lat'],
             lng=d['lng'],
         )
+
+
+class Markdown:
+    ALIGN_LEFT = ':---'
+    ALIGN_RIGHT = '---:'
+    # ALIGN_CENTER = ':---:'
+
+    @staticmethod
+    def italic(text):
+        return f'*{text}*'
+
+    # @staticmethod
+    # def bold(text):
+    #     return f'**{text}**'
+
+    @staticmethod
+    def link(text, url):
+        return f'[{text}]({url})'
+
+    @staticmethod
+    def image(alt, url):
+        return f'![{alt}]({url})'
+
+    @staticmethod
+    def table_row(cells):
+        inner = ' | '.join([str(cell) for cell in cells])
+        return f'| {inner} |'
+
+    @staticmethod
+    def table(*cells_list):
+        first_cells = cells_list[0]
+        for cells in cells_list[1:]:
+            if len(cells) != len(first_cells):
+                raise ValueError(
+                    'All rows must have the same number of cells'
+                )
+
+        return '\n'.join([Markdown.table_row(cells) for cells in cells_list])
