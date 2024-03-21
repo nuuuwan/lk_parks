1accccc7517738a9c88b02828c12e356f75884cf
nuuuwan <nuuuwan@gmail.com>
2024-03-21 11:30:21 +0530

    various

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Plants of Vihaaramahadevi Park :sri_lanka:
 
-*This analysis was automatically generated on  **Mar 21, 2024 (10:52 AM)**, and is based on  **883** plant photos.*
+*This analysis was automatically generated on  **Mar 21, 2024 (11:29 AM)**, and is based on  **883** plant photos.*
 
 Results can be directly inspected using [this app](https://nuuuwan.github.io/plants)
 
@@ -98,59 +98,9 @@ Viharamahadevi Park is 24.27ha, and has an estimated [green cover](https://en.wi
 | 20 | *Syzygium cumini* | 9 | 1.0% |
 |  | *(All Others)* | 494 | 55.9% |
 
-## Identification Confidence (by Species with at least 5 Photos)
+## Identification Confidence
 
-| Species | n(Photos) | Confidence (25th pctl.) | Confidence (Median) | Confidence (75th pctl.) |
-|:---|---:|---:|---:|---:|
-| *Anacardium occidentale* | 7 | 66.9% | 92.4% | 94.9% |
-| *Tabernaemontana divaricata* | 14 | 60.7% | 82.0% | 97.3% |
-| *Terminalia catappa* | 13 | 50.3% | 88.2% | 93.0% |
-| *Tecoma stans* | 16 | 55.0% | 69.0% | 76.7% |
-| *Monoon longifolium* | 9 | 49.2% | 71.0% | 85.4% |
-| *Tectona grandis* | 20 | 26.2% | 77.9% | 93.9% |
-| *Filicium decipiens* | 7 | 12.5% | 77.9% | 83.6% |
-| *Artocarpus heterophyllus* | 16 | 24.5% | 78.3% | 83.5% |
-| *Casuarina equisetifolia* | 7 | 38.1% | 47.9% | 75.0% |
-| *Mesua ferrea* | 45 | 40.8% | 57.1% | 70.9% |
-| *Plumeria rubra* | 9 | 42.8% | 60.8% | 65.5% |
-| *Mangifera indica* | 25 | 27.3% | 55.7% | 81.0% |
-| *Pongamia pinnata* | 18 | 12.2% | 56.4% | 82.6% |
-| *Saraca indica* | 6 | 26.3% | 52.2% | 66.8% |
-| *Terminalia arjuna* | 42 | 15.4% | 35.7% | 70.6% |
-| *Bauhinia purpurea* | 6 | 16.7% | 48.3% | 75.3% |
-| *Cassia grandis* | 5 | 25.4% | 27.6% | 68.2% |
-| *Ficus religiosa* | 16 | 11.8% | 19.7% | 89.1% |
-| *Azadirachta indica* | 12 | 12.4% | 45.1% | 67.3% |
-| *Cocos nucifera* | 6 | 3.9% | 46.0% | 78.7% |
-| *Tabebuia rosea* | 11 | 11.5% | 40.8% | 59.6% |
-| *Samanea saman* | 8 | 13.5% | 35.4% | 84.4% |
-| *Cassia fistula* | 21 | 9.0% | 27.4% | 59.5% |
-| *Mimusops elengi* | 9 | 7.5% | 11.2% | 69.6% |
-| *Calophyllum inophyllum* | 6 | 13.3% | 27.3% | 38.5% |
-| *Syzygium cumini* | 9 | 10.1% | 24.1% | 42.2% |
-| *Pterocarpus indicus* | 15 | 14.0% | 24.3% | 50.2% |
-| *Ficus benghalensis* | 14 | 10.4% | 21.5% | 48.5% |
-| *Delonix regia* | 10 | 5.6% | 39.9% | 47.8% |
-| *Lagerstroemia speciosa* | 9 | 3.3% | 17.5% | 31.9% |
-| *Schleichera oleosa* | 5 | 22.3% | 24.2% | 26.1% |
-| *Khaya senegalensis* | 6 | 10.5% | 13.4% | 28.0% |
-| *Swietenia mahagoni* | 5 | 16.8% | 20.4% | 39.1% |
-| *Madhuca longifolia* | 9 | 16.3% | 25.2% | 33.4% |
-| *Holarrhena pubescens* | 6 | 9.4% | 18.5% | 43.7% |
-| *Peltophorum pterocarpum* | 21 | 13.1% | 21.6% | 31.6% |
-| *Elaeis guineensis* | 7 | 11.6% | 17.3% | 36.6% |
-| *Jacaranda mimosifolia* | 9 | 14.7% | 19.5% | 22.9% |
-| *Areca triandra* | 5 | 8.2% | 18.9% | 19.6% |
-| *Schizolobium parahyba* | 5 | 1.8% | 12.5% | 30.0% |
-| *Psidium cattleyanum* | 5 | 6.1% | 7.9% | 11.4% |
-| *Manilkara zapota* | 7 | 5.3% | 12.1% | 28.3% |
-| *Ficus virens* | 10 | 3.7% | 18.8% | 22.7% |
-| *Peltophorum dubium* | 5 | 14.0% | 14.6% | 20.6% |
-| *Eucalyptus robusta* | 5 | 3.3% | 4.7% | 6.6% |
-| *Toona ciliata* | 10 | 3.2% | 12.0% | 17.1% |
-| *Syzygium jambos* | 5 | 3.1% | 3.1% | 16.3% |
-| *Podocarpus neriifolius* | 8 | 3.5% | 6.0% | 15.3% |
-| *Melaleuca viminalis* | 7 | 2.9% | 3.3% | 10.7% |
+![](charts/identification_score.species.png)
 
 ## Pairs of Plant Species, likely confused during identification (at least 2 times)
 
--- src/lk_plants/analysis/IdentificationReport.py
+++ src/lk_plants/analysis/IdentificationReport.py
@@ -1,13 +1,20 @@
+import os
 from functools import cached_property
-
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from utils import Log
-
+from utils_future import Markdown
 from lk_plants.core.plant_net.PlantNetResult import PlantNetResult
 
 log = Log('IdentificationReport')
 
 
 class IdentificationReport:
+    MIN_PHOTOS = 10
+    @staticmethod
+    def get_short_species_name(x):
+        words = x.split(' ')
+        return words[0][0] + '. ' + words[1]
     @cached_property
     def sorted_species_name_and_score_list(self):
         species_name_to_score_list = {}
@@ -25,39 +32,59 @@ class IdentificationReport:
         sorted_species_name_and_score_list = sorted(
             species_name_to_score_list.items(),
             key=lambda x: sum(x[1]) / len(x[1]),
-            reverse=True,
+
         )
         
         return sorted_species_name_and_score_list
 
     @cached_property
-    def lines_identification_score(self):
-        MIN_PHOTOS = 5
-        lines = [
-            '## Identification Confidence '
-            + f'(by Species with at least {MIN_PHOTOS} Photos)',
-            '',
-            '| Species | n(Photos) | Confidence (25th pctl.) '
-            + '| Confidence (Median) | Confidence (75th pctl.) |',
-            '|:---|---:|---:|---:|---:|',
-        ]
-
+    def indentification_score_data(self):
+        COLORS = ['#f008', '#f808', '#0808']
+        n_colors = len(COLORS)
+        x =[]
+        y = []
+        colors = []
         for species_name, scores in self.sorted_species_name_and_score_list:
             n = len(scores)
-            if n < MIN_PHOTOS:
+            if n < IdentificationReport.MIN_PHOTOS:
                 continue
+            for i, score in enumerate(sorted(scores)):
+                i_color = int(n_colors * i / n)
+                color = COLORS[i_color]
+                species_name_short = self.get_short_species_name(species_name)
+                x.append(species_name_short)
+                y.append(score)
+                colors.append(color)
+        return x, y, colors
+    
+    @cached_property
+    def lines_identification_score_chart(self):
+        x, y,colors = self.indentification_score_data
+        
+        plt.scatter(y ,x,color=colors)
+        title = ''
+        
+        plt.title('Identification Confidence - By Species '+f'(≥{IdentificationReport.MIN_PHOTOS} photos)')
+        ax = plt.gca()
+        ax.xaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+        plt.tight_layout()
+        
+        chart_image_path = os.path.join('charts', 'identification_score.species.png')
+        plt.savefig(chart_image_path)
+        log.info(f'Wrote {chart_image_path}')
+        
+        chart_image_path_unix = chart_image_path.replace('\\', '/')
+        return [Markdown.image(title, chart_image_path_unix)]
 
-            i_low = int(n * 0.25)
-            i_mid = int(n * 0.5)
-            i_high = int(n * 0.75)
-            sorted_scores = sorted(scores)
-            low = sorted_scores[i_low]
-            mid = sorted_scores[i_mid]
-            high = sorted_scores[i_high]
 
-            lines.append(
-                f'| *{species_name}* | {n:,} | {low:.1%} '
-                + f'| {mid:.1%} | {high:.1%} |'
-            )
+    @cached_property
+    def lines_identification_score(self):
+       
+        lines = [
+            '## Identification Confidence',
+            '',
+        ]
+
+        lines.extend(self.lines_identification_score_chart)
         lines.append('')
         return lines
