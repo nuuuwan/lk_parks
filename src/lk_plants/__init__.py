c722f883018c4933090ede921bf8b521ef8c685f
nuuuwan <nuuuwan@gmail.com>
2024-03-23 10:01:37 +0530

    Ran pipeline

--- src/lk_plants/__init__.py
+++ src/lk_plants/__init__.py
@@ -1,5 +1,6 @@
-from lk_plants.analysis.ViharamahadeviParkReport import \
-    ViharamahadeviParkReport
+from lk_plants.analysis.ViharamahadeviParkReport import (
+    ViharamahadeviParkReport,
+)
 from lk_plants.core.data_app.DataApp import DataApp
 from lk_plants.core.misc.NameTranslator import NameTranslator
 from lk_plants.core.plant_net.PlantNetResult import PlantNetResult
