d033fb5fe98409d1afcf2b546664340df5d1bdce
nuuuwan <nuuuwan@gmail.com>
2024-03-26 12:40:45 +0530

    Ran pipeline_test

--- src/lk_plants/analysis/ReadMeDifficultIds.py
+++ src/lk_plants/analysis/ReadMeDifficultIds.py
@@ -11,12 +11,22 @@ class ReadMeDifficultIds(MarkdownPage, InfoReadMe):
     def file_path(self):
         return 'README.difficult_ids.md'
 
+    @staticmethod
+    def is_difficult(plant_photo: PlantPhoto) -> bool:
+        plant_net_result = PlantNetResult.from_plant_photo(plant_photo)
+        species_name_to_score = plant_net_result.species_name_to_score
+        if not species_name_to_score:
+            return False
+        score = list(species_name_to_score.values())[0]
+        return score < InfoReadMe.MIN_CONFIDENCE
+            
+
     @cached_property
     def lines_inner(self) -> list[str]:
         plant_photos = PlantPhoto.list_all()
         plant_photos_difficult = [
             plant_photo for plant_photo in plant_photos
-            if list(PlantNetResult.from_plant_photo(plant_photo).species_name_to_score.values())[0] < InfoReadMe.MIN_CONFIDENCE
+            if ReadMeDifficultIds.is_difficult(plant_photo)
         ]
         random.shuffle(plant_photos_difficult)
         N_DISPLAY = 20
