df87702b8eacc019a04b4d75a64d8536017c721c
nuuuwan <nuuuwan@gmail.com>
2024-03-26 17:16:09 +0530

    Added App

--- src/lk_plants/__init__.py
+++ src/lk_plants/__init__.py
@@ -4,3 +4,4 @@ from lk_plants.core.misc.NameTranslator import NameTranslator
 from lk_plants.core.plant_net.PlantNetResult import PlantNetResult
 from lk_plants.core.plant_photo.PlantPhoto import PlantPhoto
 from lk_plants.core.wiki.WikiPage import WikiPage
+from lk_plants.app.App import App
---
+++ src/lk_plants/app/App.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from utils import Log
+
+log = Log('App')
+
+
+class App:
+    @staticmethod 
+    def scrape():
+        options = Options()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
+        width = 1600
+        height = width * 9 / 16
+        driver.set_window_size(width, height)
+
+        eppId = 'Photo-2024-03-08-07-14-47'
+        URL = 'https://nuuuwan.github.io/plants/' + f'?activeEPPId={eppId}'
+        log.debug(f'Openning {URL}...')
+        driver.get(URL)
+        time.sleep(3)
+
+        image_path = os.path.join('images', 'app.png')
+        driver.save_screenshot(image_path)
+        log.info(f'Wrote {image_path}')
+        driver.quit()
