baae5c4a4cffd3bd5f49ee98789bd6785926edc4
nuuuwan <nuuuwan@gmail.com>
2024-04-02 11:50:58 +0530

    lint

--- src/lk_plants/__init__.py
+++ src/lk_plants/__init__.py
@@ -1,57 +1,12 @@
 # lk_plants (auto generate by build_inits.py)
 
-from lk_plants.analysis import (
-    InfoReadMe,
-    ReadMe,
-)
-
-from lk_plants.analysis import (
-    ReadMeAbout,
-    ReadMeApp,
-    ReadMeDifficultIds,
-    ReadMeFunnel,
-    ReadMeFunnelByDay,
-    ReadMeIdentification,
-    ReadMeMostCommonSpecies,
-    ReadMePlantNet,
-    ReadMeStatisticsByTaxonomy,
-    ReadMeVMDPark,
-)
-
-from lk_plants.app import (
-    App,
-)
-
-from lk_plants.core import (
-    DataApp,
-)
-
-from lk_plants.core import (
-    NameTranslator,
-)
-
-from lk_plants.core import (
-    PlantNetResult,
-)
-
-from lk_plants.core import (
-    PlantPhoto,
-    PlantPhotoBase,
-    PlantPhotoOriginalImage,
-)
-
-from lk_plants.core import (
-    Family,
-    Genus,
-    Species,
-    Taxon,
-)
-
-from lk_plants.core import (
-    WikiPage,
-)
-
-from lk_plants.twtr import (
-    Twtr,
-)
-
+from lk_plants.analysis import (InfoReadMe, ReadMe, ReadMeAbout, ReadMeApp,
+                                ReadMeDifficultIds, ReadMeFunnel,
+                                ReadMeFunnelByDay, ReadMeIdentification,
+                                ReadMeMostCommonSpecies, ReadMePlantNet,
+                                ReadMeStatisticsByTaxonomy, ReadMeVMDPark)
+from lk_plants.app import App
+from lk_plants.core import (DataApp, Family, Genus, NameTranslator,
+                            PlantNetResult, PlantPhoto, PlantPhotoBase,
+                            PlantPhotoOriginalImage, Species, Taxon, WikiPage)
+from lk_plants.twtr import Twtr
--- src/lk_plants/core/__init__.py
+++ src/lk_plants/core/__init__.py
@@ -1,31 +1,9 @@
 # lk_plants.core (auto generate by build_inits.py)
 
-from lk_plants.core.data_app import (
-    DataApp,
-)
-
-from lk_plants.core.misc import (
-    NameTranslator,
-)
-
-from lk_plants.core.plant_net import (
-    PlantNetResult,
-)
-
-from lk_plants.core.plant_photo import (
-    PlantPhoto,
-    PlantPhotoBase,
-    PlantPhotoOriginalImage,
-)
-
-from lk_plants.core.taxonomy import (
-    Family,
-    Genus,
-    Species,
-    Taxon,
-)
-
-from lk_plants.core.wiki import (
-    WikiPage,
-)
-
+from lk_plants.core.data_app import DataApp
+from lk_plants.core.misc import NameTranslator
+from lk_plants.core.plant_net import PlantNetResult
+from lk_plants.core.plant_photo import (PlantPhoto, PlantPhotoBase,
+                                        PlantPhotoOriginalImage)
+from lk_plants.core.taxonomy import Family, Genus, Species, Taxon
+from lk_plants.core.wiki import WikiPage
