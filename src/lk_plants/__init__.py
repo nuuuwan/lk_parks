5e9f2ce08c2f86accc5dd55dd9657f6a6bd21a2a
nuuuwan <nuuuwan@gmail.com>
2024-04-05 09:22:12 +0530

    Ran pipeline

--- src/lk_plants/__init__.py
+++ src/lk_plants/__init__.py
@@ -1,14 +1,67 @@
 # lk_plants (auto generate by build_inits.py)
 
-from lk_plants.analysis import (InfoReadMe, ReadMe, ReadMeAbout, ReadMeApp,
-                                ReadMeDifficultIds, ReadMeFunnel,
-                                ReadMeFunnelByDay, ReadMeIdentification,
-                                ReadMeMostCommonSpecies, ReadMePlantNet,
-                                ReadMeStatisticsByTaxonomy, ReadMeSunburst,
-                                ReadMeVMDPark)
-from lk_plants.app import App
-from lk_plants.core import (GBIF, Classis, DataApp, Domain, Family, Genus,
-                            Kingdom, NameTranslator, Order, Phylum,
-                            PlantNetResult, PlantPhoto, PlantPhotoBase,
-                            PlantPhotoOriginalImage, Species, Taxon, WikiPage)
-from lk_plants.twtr import Twtr
+from lk_plants.analysis import (
+    InfoReadMe,
+    ReadMe,
+)
+
+from lk_plants.analysis import (
+    ReadMeAbout,
+    ReadMeApp,
+    ReadMeDifficultIds,
+    ReadMeFunnel,
+    ReadMeFunnelByDay,
+    ReadMeIdentification,
+    ReadMeMostCommonSpecies,
+    ReadMePlantNet,
+    ReadMeStatisticsByTaxonomy,
+    ReadMeSunburst,
+    ReadMeVMDPark,
+)
+
+from lk_plants.app import (
+    App,
+)
+
+from lk_plants.core import (
+    DataApp,
+)
+
+from lk_plants.core import (
+    GBIF,
+)
+
+from lk_plants.core import (
+    NameTranslator,
+)
+
+from lk_plants.core import (
+    PlantNetResult,
+)
+
+from lk_plants.core import (
+    PlantPhoto,
+    PlantPhotoBase,
+    PlantPhotoOriginalImage,
+)
+
+from lk_plants.core import (
+    Classis,
+    Domain,
+    Family,
+    Genus,
+    Kingdom,
+    Order,
+    Phylum,
+    Species,
+    Taxon,
+)
+
+from lk_plants.core import (
+    WikiPage,
+)
+
+from lk_plants.twtr import (
+    Twtr,
+)
+
--- src/lk_plants/core/__init__.py
+++ src/lk_plants/core/__init__.py
@@ -1,11 +1,40 @@
 # lk_plants.core (auto generate by build_inits.py)
 
-from lk_plants.core.data_app import DataApp
-from lk_plants.core.gbif import GBIF
-from lk_plants.core.misc import NameTranslator
-from lk_plants.core.plant_net import PlantNetResult
-from lk_plants.core.plant_photo import (PlantPhoto, PlantPhotoBase,
-                                        PlantPhotoOriginalImage)
-from lk_plants.core.taxonomy import (Classis, Domain, Family, Genus, Kingdom,
-                                     Order, Phylum, Species, Taxon)
-from lk_plants.core.wiki import WikiPage
+from lk_plants.core.data_app import (
+    DataApp,
+)
+
+from lk_plants.core.gbif import (
+    GBIF,
+)
+
+from lk_plants.core.misc import (
+    NameTranslator,
+)
+
+from lk_plants.core.plant_net import (
+    PlantNetResult,
+)
+
+from lk_plants.core.plant_photo import (
+    PlantPhoto,
+    PlantPhotoBase,
+    PlantPhotoOriginalImage,
+)
+
+from lk_plants.core.taxonomy import (
+    Classis,
+    Domain,
+    Family,
+    Genus,
+    Kingdom,
+    Order,
+    Phylum,
+    Species,
+    Taxon,
+)
+
+from lk_plants.core.wiki import (
+    WikiPage,
+)
+
